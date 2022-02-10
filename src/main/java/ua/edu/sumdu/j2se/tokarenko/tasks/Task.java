ae8c7d46f62fa8c6aa7c53da00d76c6065039522
FFIRELESS <dimatok006@gmail.com>
2022-02-10 12:39:02 +0200

    added constructors

---
+++ src/main/java/ua/edu/sumdu/j2se/tokarenko/tasks/Task.java
@@ -0,0 +1,9 @@
+package ua.edu.sumdu.j2se.tokarenko.tasks;
+
+public class Task {
+    public Task(String title, int time) {
+    }
+    public Task(String title, int start, int end, int interval) {
+
+    }
+}
