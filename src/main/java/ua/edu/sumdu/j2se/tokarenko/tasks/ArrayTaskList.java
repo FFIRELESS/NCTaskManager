e594c2e198c341f050ef0740f8eaf3fcaea5e0af
FFIRELESS <dimatok006@gmail.com>
2022-02-17 12:43:06 +0200

    completed pract_3

---
+++ src/main/java/ua/edu/sumdu/j2se/tokarenko/tasks/ArrayTaskList.java
@@ -0,0 +1,73 @@
+package ua.edu.sumdu.j2se.tokarenko.tasks;
+
+public class ArrayTaskList {
+    private Task[] tasks;
+    public int taskAmount;
+    private final int INTERVAL = 5;
+
+    public ArrayTaskList() {
+        tasks = new Task[INTERVAL];
+    }
+
+    public int size() {
+        return taskAmount;
+    }
+
+    public void add(Task task) {
+        if (tasks.length == taskAmount) {
+            Task[] tempArr = new Task[taskAmount + INTERVAL];
+
+            System.arraycopy(tasks, 0, tempArr, 0, taskAmount);
+            tasks = tempArr;
+        }
+        tasks[taskAmount++] = task;
+    }
+
+    public boolean remove(Task task) {
+        boolean status = false;
+        int index_to_del;
+
+        for (index_to_del = 0; index_to_del < taskAmount; index_to_del++) {
+            if (tasks[index_to_del].equals(task)) {
+                status = true;
+                break;
+            }
+        }
+
+        if (!status) {
+            return false;
+        }
+
+        tasks[index_to_del] = null;
+        taskAmount--;
+
+        if (index_to_del != taskAmount) {
+            System.arraycopy(tasks, index_to_del + 1, tasks, index_to_del, taskAmount - index_to_del);
+        }
+
+        if (tasks.length - INTERVAL == taskAmount && taskAmount != 0) {
+            Task[] tempArray = new Task[taskAmount];
+            System.arraycopy(tasks, 0, tempArray, 0, taskAmount);
+            tasks = tempArray;
+        }
+        return true;
+    }
+
+    public Task getTask(int index) {
+        return tasks[index];
+    }
+
+    public ArrayTaskList incoming(int from, int to) {
+        int nextTaskTime;
+        ArrayTaskList array = new ArrayTaskList();
+
+        for (int i = 0; i < taskAmount; i++) {
+            nextTaskTime = tasks[i].nextTimeAfter(from);
+
+            if (nextTaskTime != -1 && nextTaskTime < to) {
+                array.add(tasks[i]);
+            }
+        }
+        return array;
+    }
+}
